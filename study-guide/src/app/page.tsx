9efb3236e203bb9c8e1d90c9ede6bc3a7043386d
Estevão Gomes <estevaog.gomes@gmail.com>
2023-08-01 21:05:43 +0000

    [Style]Creates skeleton

--- study-guide/src/app/page.tsx
+++ study-guide/src/app/page.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center">
-      <h1 className='text-3xl'>Study Guide</h1>
-      <div>
-        
+    <main className="flex min-h-screen flex-col items-center mt-8">
+      <h1 className="text-3xl my-4">Study Guide</h1>
+      <div className="border border-red-400 rounded-md w-full">Menu</div>
+      <div className="w-full p-4 border border-green-400 rounded-md">
+        Container
       </div>
     </main>
-  )
+  );
 }
