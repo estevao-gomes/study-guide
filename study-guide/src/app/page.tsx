cece490a3fba7cc1a307a9f955e95f55570eee26
Estevão Gomes <estevaog.gomes@gmail.com>
2023-08-01 20:58:45 +0000

    First Commit

---
+++ study-guide/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-center">
+      <h1 className='text-3xl'>Study Guide</h1>
+      <div>
+        
+      </div>
+    </main>
+  )
+}
